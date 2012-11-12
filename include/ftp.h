befd7ad868bfb6f6e072c4a3d0208787b934c31b
André Freitas <p.andrefreitas@gmail.com>
2012-11-12 22:11:39 +0000

    Ja consegue buscar os argumentos de uma maneira mais robusta

--- include/ftp.h
+++ include/ftp.h
@@ -5,7 +5,7 @@
 #define MAXSIZE 256
 #define ALLOCSTRING (char *) malloc(sizeof(char)*MAXSIZE)
 
-int parseParams(char *argv,char *user,char *password, char *host, char *url);
+int parseParams(char *argv,char *user,char *password, char *host, char *urlpath);
 
 int download(char *argv);
 
--- src/ftp.c
+++ src/ftp.c
@@ -11,17 +11,49 @@ int download(char* argv) {
 	return 0;
 }
 
-int parseParams(char *argv, char *user, char *password, char *host, char *url) {
-	// ftp://user:password@host/urlpath
+int parseParams(char *argv, char *user, char *password, char *host, char *urlpath) {
+	// argv = ftp://user:password@host/urlpath
+	// argv = ftp://host/urlpath
+	// argv = ftp://@host.com
+	// argv = ftp://foo:@host.com
 
 	char *temp, *temp2;
 	int len;
 
 	temp = strchr(argv, '/') + 2;
-	temp2 = strchr(temp, ':') - 1;
-	len=temp2-temp+1;
+	temp2 = strchr(temp, ':');
 
-	printf("userlen: %d\n",len);
+	// If needs authentication
+	if (temp2 != 0) {
+		temp2--;
+
+		// user
+		len = temp2 - temp + 1;
+		strncpy(user, temp, len);
+
+		// password
+		temp2 += 2;
+		temp = strchr(temp2, '@')-1;
+		len = temp - temp2 + 1;
+		strncpy(password, temp2, len);
+
+		temp+=2;
+
+		printf("User: %s Password: %s ",user,password);
+		if(strlen(password)==0) password=0;
+	}
+
+	// host
+	temp2 = strchr(temp,'/')-1;
+	len = temp2-temp+1;
+	strncpy(host, temp, len);
+	if(host[0]=='@') host++;
+
+	// urlpath
+	temp2+=2;
+	strcpy(urlpath,temp2);
+
+	printf("Host: %s Urlpath: %s\n",host,urlpath);
 	return 0;
 
 }
