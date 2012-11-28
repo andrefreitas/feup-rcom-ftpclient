9548d96b1f3ef68715264fc0faec8d0eedee6bb7
André Freitas <p.andrefreitas@gmail.com>
2012-11-28 21:30:07 +0000

    Corrigida a expressão regular para aceitar ficheiros com extensão e a função de parsing dos parâmetros devolve códigos de retorno consoante os parâmetros dados. Assim, se deu oo user e password o return é 0, se deu apenas o user o return é 1 e se não deu nem user nem password o return é 2.

--- src/ftp.c
+++ src/ftp.c
@@ -13,8 +13,16 @@ int download(char* argv) {
 
 int parseParams(char *argv, char *user, char *password, char *host,
 		char *urlpath) {
+
+	// argv = ftp://user:password@host.com/urlpath
+	// argv = ftp://host.com/urlpath
+	// argv = ftp://@host.com
+	// argv = ftp://foo:@host.com
+
 	char *temp, *temp2;
-	int len;
+	int len, ret = 0;
+
+
 
 	temp = strchr(argv, '/') + 2;
 	temp2 = strchr(temp, ':');
@@ -26,24 +34,30 @@ int parseParams(char *argv, char *user, char *password, char *host,
 		// user
 		len = temp2 - temp + 1;
 		strncpy(user, temp, len);
+		user[len]='\0';
 
 		// password
 		temp2 += 2;
 		temp = strchr(temp2, '@') - 1;
 		len = temp - temp2 + 1;
 		strncpy(password, temp2, len);
+		password[len]='\0';
 
 		temp += 2;
 
 		printf("User: %s Password: %s ", user, password);
-		if (strlen(password) == 0)
-			password = 0;
+		if (strlen(password) == 0){
+			ret=1;
+		}
+	}else{
+		ret=2;
 	}
 
 	// host
 	temp2 = strchr(temp, '/') - 1;
 	len = temp2 - temp + 1;
 	strncpy(host, temp, len);
+	host[len]='\0';
 	if (host[0] == '@')
 		host++;
 
@@ -52,15 +66,14 @@ int parseParams(char *argv, char *user, char *password, char *host,
 	strcpy(urlpath, temp2);
 
 	printf("Host: %s Urlpath: %s\n", host, urlpath);
-	return 0;
+	return ret;
 
 }
 
 int checkSyntax(char *argv) {
-	// re = ftp://(([a-zA-Z0-9.]+:[a-zA-Z0-9]*@)?|@?)([a-zA-Z0-9]+[.])+[a-zA-Z0-9]+/([a-zA-Z0-9]+[/]?)*
-	// argv = ftp://user:password@host/urlpath
-	// argv = ftp://host/urlpath
-	// argv = ftp://@host.com
+	// re = ftp://(([a-zA-Z0-9.]+:[a-zA-Z0-9]*@)?|@?)([a-zA-Z0-9]+[.])+[a-zA-Z0-9]+/([a-zA-Z0-9]+[/]?)*([.][a-zA-Z0-9]+)?
+	// argv = ftp://user:password@host.com/urlpath
+	// argv = ftp://host.com/urlpath
 	// argv = ftp://foo:@host.com
 
 	regex_t regex;
@@ -68,7 +81,7 @@ int checkSyntax(char *argv) {
 	size_t nmatch=strlen(argv);
 	regmatch_t pmatch[nmatch];
 
-	regcomp(&regex, "ftp://\\(\\([a-zA-Z0-9\\.]\\+:[a-zA-Z0-9]*@\\)\\?\\|@\\?\\)\\([a-zA-Z0-9]\\+[\\.]\\)\\+[a-zA-Z0-9]\\+/\\([a-zA-Z0-9]\\+[/]\\?\\)*", 0);
+	regcomp(&regex, "ftp://(([a-zA-Z0-9.]+:[a-zA-Z0-9]*@)?|@?)([a-zA-Z0-9]+[.])+[a-zA-Z0-9]+/([a-zA-Z0-9]+[/]?)*([.][a-zA-Z0-9]+)?",REG_EXTENDED);
 	regexec(&regex, argv, nmatch, pmatch, 0);
 
 	totalMatch=pmatch[0].rm_eo;
--- src/main.c
+++ src/main.c
@@ -6,6 +6,14 @@ int main(int argc, char *argv[]){
 		int correctSyntax=checkSyntax(argv[1]);
 		if(correctSyntax){
 			printf("Sintaxe correcta\n");
+
+			char *user,*password,*host,*urlpath;
+
+			user=ALLOCSTRING;
+			password=ALLOCSTRING;
+			host=ALLOCSTRING;
+			urlpath=ALLOCSTRING;
+			printf("Ret: %d\n",parseParams(argv[1],user,password,host,urlpath));
 		}
 		else printf("Sintaxe errada!\n");
 
